ed24cee8138886023f8f2fc2cb1b045945a16195
sara-cwru <sfl21@case.edu>
2021-04-26 17:50:10 -0400

    test

--- Project/website/models.py
+++ Project/website/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class MenuItem(model.Model):
     menu_item_name = models.CharField(max_length=200)
     menu_item_description = models.CharField(max_length=200)
+    menu_item_price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.question_text
