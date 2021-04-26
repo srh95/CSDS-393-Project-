5eb92144f8f605d4f210e85af220831b5550648c
sara-cwru <sfl21@case.edu>
2021-04-26 17:26:11 -0400

    added menu item model

--- Project/website/models.py
+++ Project/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class MenuItem(model.Model):
+    menu_item_name = models.CharField(max_length=200)
+    menu_item_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
 # Create your models here.
