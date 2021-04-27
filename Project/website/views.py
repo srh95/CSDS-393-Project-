3188a61ee16af925227a19b8efaac40e7771fe06
sara-cwru <sfl21@case.edu>
2021-04-27 15:00:52 -0400

    Added menu item template

--- Project/website/views.py
+++ Project/website/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import MenuItem
 from django.template import loader
+from django.shortcuts import get_object_or_404
 
 def index(request):
     return HttpResponse("Hello, world. You're at the website index.")
 
-def menu_item(request, name):
-    return HttpResponse("You're looking at menu item %s." % name)
+def menu_item(request, menu_item_id):
+    menu_item = get_object_or_404(MenuItem, pk=menu_item_id)
+    return render(request, 'website/menu_item.html', {'menu_item': menu_item})
 
 def menu_list(request):
     menu_list = MenuItem.objects.all()
