97c34009555532bd6f2e9d81e6423b9e1d4b1720
Olivia Weng <osw9@case.edu>
2021-04-28 02:05:51 -0400

    registration form

---
+++ Project/website/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+class RegisterForm(forms.Form):
+    restaurantname = forms.CharField(label='Restaurant Name',max_length=50, required=True)
+    username = forms.CharField(label='username', max_length=50, required=True)
+    password1 = forms.CharField(label='password', max_length=50, required=True)
+    password2 = forms.CharField(label='password*', max_length=50, required=True)
+
+
+    # def clean(self):
+    #     cleaned_data = self.cleaned_data
+    #     password1 = cleaned_data.get("password1")
+    #     password2 = cleaned_data.get("password2")
+    #     if password1 != password2:
+    #         raise forms.ValidationError("Passwords do not match")
+        
+    #     return cleaned_data
+
+
+
+    
+#	fields = form ["restaurant name", "username", "password1", "password2"]
--- Project/website/views.py
+++ Project/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import MenuItem
 from django.template import loader
 from django.shortcuts import get_object_or_404
 from .models import Restaurant
+from .forms import RegisterForm
+from django.core.exceptions import ValidationError
 
 def index(request):
     return HttpResponse("Hello, world. You're at the website index.")
@@ -21,3 +24,21 @@ def restaurant(request, restaurant_id):
     restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
     context = {'restaurant' : restaurant}
     return render(request, 'website/restaurant.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            if(form.cleaned_data['password1'] != form.cleaned_data['password2']):
+                raise ValidationError('Passwords do not match')
+            database = Restaurant.objects.create(
+            restaurant_name = form.cleaned_data['restaurantname'],
+            restaurant_username = form.cleaned_data['username'],
+            restaurant_password = form.cleaned_data['password1']
+            )
+            database.save()
+            return HttpResponseRedirect('/website/accounts/login/')
+    else:
+        form = RegisterForm()
+        context = {'form' : form}
+    return render(request, 'website/register.html', context)
