67a5da412ec10ebe3bf8ff7fe565abcf1c74ed42
sara-cwru <sfl21@case.edu>
2021-04-27 22:14:28 -0400

    Connected menu item with restaurant

--- Project/website/models.py
+++ Project/website/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+class Restaurant(models.Model):
+    #the restaurant's name and login info
+    restaurant_name = models.CharField(max_length=200)
+    restaurant_username = models.CharField(max_length=30)
+    restaurant_password = models.CharField(max_length=50)
+
 class MenuItem(models.Model):
     # the specified restaurant would be the foreign key to connect menu items with the restaurants
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     menu_item_name = models.CharField(max_length=200)
     menu_item_description = models.CharField(max_length=200)
     menu_item_price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.menu_item_name
-
-class Restaurant(models.Model):
-    #the restaurant's name and login info
-    restaurant_name = models.CharField(max_length=200)
-    restaurant_username = models.CharField(max_length=30)
-    restaurant_password = models.CharField(max_length=50)
 
 # Create your models here.
